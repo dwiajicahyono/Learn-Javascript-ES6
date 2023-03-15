1bde56383d3e76ddb90ca9da2b4ab962373f9c60
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-03-16 04:30:55 +0700

    promise - setTimeout

---
+++ promise - setTimeout.js
@@ -0,0 +1,12 @@
+console.log("Selamat pagi, dwi");
+
+setTimeout(() =>{
+    console.log("Terimakasih dwi atas kunjungannya")
+}, 3000);
+
+console.log("Silahkan stay disini untuk beberapa saat");
+
+/*
+Yang dieksekusi adalah baris ke 1 lalu 7 terlebih dahulu baru yang terakhir yaitu fungsi
+timeoutnya maka dari itu ini yang disebut oleh js asynchronous
+*/ 
